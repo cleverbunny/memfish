98249a7a6c1ff5098dbdcadf09407af8a09ae4ff
Tetiana Dushenkivska <myemail@here>
2019-01-02 02:41:06 +0000

    Implement storing, retrieving and forgetting values by key.
    Memfish provides you a way to store values against the key for a short period of time.

--- lib/memfish/application.ex
+++ lib/memfish/application.ex
@@ -1,19 +1,11 @@
 defmodule Memfish.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
-    children = [
-      # Starts a worker by calling: Memfish.Worker.start_link(arg)
-      # {Memfish.Worker, arg},
-    ]
+    children = [Memfish]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Memfish.Supervisor]
     Supervisor.start_link(children, opts)
   end
